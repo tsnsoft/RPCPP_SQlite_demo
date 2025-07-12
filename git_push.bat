4cc11293e298531c52a02e9905760ce8a6fdcbf7
Talipov S.N. <proffix4@gmail.com>
2025-07-12 13:14:31 +0500

    rev. fallback_commit

--- git_push.bat
+++ git_push.bat
@@ -1,22 +1,22 @@
 @echo off
 cd /d "%~dp0"
 
-echo Adding all changes...
+echo Добавление всех изменений...
 git add .
 
-:: Capture date and time
+:: Пол rakaia date and time
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set "datetime=%%I"
 if not defined datetime (
-    echo Error: Could not retrieve date and time.
+    echo Ошибка: Не удалось получить дату и время.
     set "commit_msg=rev. fallback_commit"
 ) else (
     set "commit_msg=rev. %datetime:~0,8%_%datetime:~8,6%"
 )
 
-echo Creating commit with message: %commit_msg%
+echo Создание коммита с сообщением: %commit_msg%
 git commit -m "%commit_msg%"
 
-echo Pushing to GitHub...
+echo Отправка на GitHub...
 git push origin main
 
-echo Done!
+echo Готово!
