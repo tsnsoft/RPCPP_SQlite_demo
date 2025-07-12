b59cdf239ece64464f9571125a177c55f63036ac
Talipov S.N. <proffix4@gmail.com>
2025-07-12 13:10:23 +0500

    rev. ~6,2.~4,2.~0,4 ~8,2:~10,2

--- git_push.bat
+++ git_push.bat
@@ -4,11 +4,25 @@ cd /d "%~dp0"
 echo Adding all changes...
 git add .
 
-for /f "tokens=2 delims==." %%I in ('wmic os get localdatetime /value') do set datetime=%%I
-set commit_msg=rev. %datetime%
+:: Получаем дату и время из WMIC
+for /f "tokens=2 delims==." %%I in ('wmic os get localdatetime /value') do (
+    set datetime=%%I
+)
 
-echo Creating commit...
-git commit -m "%commit_msg%"
+setlocal enabledelayedexpansion
+
+:: Извлекаем компоненты даты и времени
+set year=!datetime:~0,4!
+set month=!datetime:~4,2!
+set day=!datetime:~6,2!
+set hour=!datetime:~8,2!
+set minute=!datetime:~10,2!
+
+:: Формируем сообщение коммита в читаемом виде
+set commit_msg=rev. !day!.!month!.!year! !hour!:!minute!
+
+echo Creating commit with message "!commit_msg!"...
+git commit -m "!commit_msg!"
 
 echo Pushing to GitHub...
 git push origin main
